383ea484c157093d7a21fd60a39bf7acba714829
IT - Atiqur Rahman <atiq@citygroupbd.com>
2023-04-06 08:15:00 +0600

    second commit

--- src/components/homepagelayout/footer.tsx
+++ src/components/homepagelayout/footer.tsx
@@ -15,6 +15,7 @@ const Footer = () => {
       <Box sx={{ display: "flex", justifyContent: "space-between", width: "50%" }}>
         <Button sx={{ color: "#fff", fontSize: "1.2rem" }}>Our Services</Button>
         <Button sx={{ color: "#fff", fontSize: "1.2rem" }}>Contact Us</Button>
+        <Button sx={{ color: "#fff", fontSize: "1.2rem" }}>email us</Button>
       </Box>
     </Box>
   );
