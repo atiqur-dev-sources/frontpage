12c7a27466e21e414db4c5becbd5b7d6d7e87ed5
IT - Atiqur Rahman <atiq@citygroupbd.com>
2023-04-07 17:33:54 +0600

    better login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,133 +1,140 @@
-import { useState } from 'react';
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Container,
-  createTheme,
-  CssBaseline,
-  FormControl,
-  FormHelperText,
-  IconButton,
-  Input,
-  InputAdornment,
-  InputLabel,
-  ThemeProvider,
-  Typography,
-} from '@mui/material';
-import { Send, ArrowBack } from '@mui/icons-material';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import axios from 'axios';
+import React from 'react';
+import { Box, Button, FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from '@mui/material';
+import * as Yup from 'yup';
+import SendIcon from '@mui/icons-material/Send';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
-const theme = createTheme();
-
-const validationSchema = yup.object({
-  employeeId: yup.string().required('Please put employee Id'),
-  password: yup.string().required('Please put your password'),
-});
-
-export default function LoginPage() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const formik = useFormik({
-    initialValues: {
-      employeeId: '',
-      password: '',
-    },
-    validationSchema,
-    onSubmit: async (values) => {
-      setIsSubmitting(true);
-      try {
-        const response = await axios.post('https://youapipath/submit', values);
-        console.log('response', response);
-      } catch (error) {
-        console.error('error', error);
-      } finally {
-        setIsSubmitting(false);
-      }
-    },
+const LoginPage = () => {
+  const [employeeId, setEmployeeId] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [errors, setErrors] = React.useState({
+    employeeId: '',
+    password: '',
   });
 
+  const handleEmployeeIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmployeeId(event.target.value);
+    setErrors({ ...errors, employeeId: '' });
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+    setErrors({ ...errors, password: '' });
+  };
+
+  const handleToggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      // call the submit API using axios
+      const response = await axios.post('https://youapipath/submit', { employeeId, password });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleBlur = () => {
+    const validationSchema = Yup.object().shape({
+      employeeId: Yup.string().required('Please put employee Id'),
+      password: Yup.string().required('Please put your password'),
+    });
+
+    validationSchema.validate({ employeeId, password }, { abortEarly: false }).catch((err) => {
+      const newErrors: { [key: string]: string } = {};
+      err.inner.forEach((e) => {
+        newErrors[e.path] = e.message;
+      });
+      setErrors(newErrors);
+    });
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container maxWidth="sm">
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+       
+      }}
+    >
+      <Typography variant="h4" align="center" gutterBottom>
+        Welcome to City Group apps
+      </Typography>
+      <Box
+        sx={{
+          border: '1px solid #ccc',
+          padding: 3,
+          maxWidth: '80%',
+          width: 500,
+          bgcolor: '#ffffff', // Set the background color of the page
+        }}
+      >
         <Box
           sx={{
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh',
+            gap: 2,
           }}
+          component="form"
+          onSubmit={handleSubmit}
         >
-          <Typography variant="h4" component="h1" mb={2}>
-            Welcome to City Group apps
-          </Typography>
-          <Box
-            component="form"
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              width: '100%',
-              '& > :not(style)': { m: 1, width: '100%' },
-            }}
-            onSubmit={formik.handleSubmit}
-            noValidate
-          >
-            <FormControl error={formik.touched.employeeId && Boolean(formik.errors.employeeId)}>
-              <InputLabel htmlFor="employee-id">Employee Id</InputLabel>
-              <Input
-                id="employee-id"
-                name="employeeId"
-                type="text"
-                value={formik.values.employeeId}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                endAdornment={
-                  formik.touched.employeeId && Boolean(formik.errors.employeeId) ? (
-                    <InputAdornment position="end">
-                      <IconButton aria-label="error">
-                        <FormHelperText>{formik.errors.employeeId}</FormHelperText>
-                      </IconButton>
-                    </InputAdornment>
-                  ) : null
-                }
-              />
-            </FormControl>
-            <FormControl error={formik.touched.password && Boolean(formik.errors.password)}>
-              <InputLabel htmlFor="password">Your Password</InputLabel>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                endAdornment={
-                  formik.touched.password && Boolean(formik.errors.password) ? (
-                    <InputAdornment position="end">
-                      <IconButton aria-label="error">
-                        <FormHelperText>{formik.errors.password}</FormHelperText>
-                      </IconButton>
-                    </InputAdornment>
-                  ) : null
-                }
-              />
-            </FormControl>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-<Button variant="contained" color="primary" startIcon={<Send />} type="submit" disabled={isSubmitting || !formik.isValid}>
-{isSubmitting ? <CircularProgress size={24} /> : 'Submit'}
-</Button>
-<Button variant="contained" color="secondary" startIcon={<ArrowBack />} onClick={() => console.log('back clicked')}>
-Back
-</Button>
-</Box>
-</Box>
-</Box>
-</Container>
-</ThemeProvider>
-);
-}
+          <FormControl variant="outlined" error={Boolean(errors.employeeId)}>
+            <InputLabel htmlFor="employee-id-input">Employee Id</InputLabel>
+            <OutlinedInput
+              id="employee-id-input"
+              value={employeeId}
+              onChange={handleEmployeeIdChange}
+              onBlur={handleBlur}
+              label="Employee Id"
+            />
+            <FormHelperText>{errors.employeeId}</FormHelperText>
+          </FormControl>
+          <FormControl variant="outlined" error={Boolean(errors.password)}>
+            <InputLabel htmlFor="password-input">Your Password</InputLabel>
+            <OutlinedInput
+              id="password-input"
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={handlePasswordChange}
+              onBlur          ={handleBlur}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton onClick={handleToggleShowPassword} edge="end">
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Your Password"
+            />
+            <FormHelperText>{errors.password}</FormHelperText>
+          </FormControl>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+           
+            <Button variant="contained" color="secondary" startIcon={<ArrowBackIcon />}>
+              Back
+            </Button>
+            <Button variant="contained" color="primary" disabled={!employeeId || !password} type="submit" startIcon={<SendIcon />}>
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+      {/* Footer text */}
+      <Box sx={{ mt: 4 }}>
+        Powered by: IT, City Group. &copy; All  rights reservered.
+      </Box>
+    </Box>
+    );
+};
 
+export default LoginPage;
+    
